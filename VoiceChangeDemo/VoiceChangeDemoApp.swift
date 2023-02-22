35374f97612948627b434893d9f95013c085ba94
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-02-22 23:56:11 +0600

    Initial Commit

---
+++ VoiceChangeDemo/VoiceChangeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceChangeDemoApp.swift
+//  VoiceChangeDemo
+//
+//  Created by Joynal Abedin on 22/2/23.
+//
+
+import SwiftUI
+
+@main
+struct VoiceChangeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
